0a15b1aa23b20f5ad41cc09d51937b0dee83bfab
Jay Shenk <jay.shenk@gmail.com>
2016-04-21 21:24:09 -0400

    Handling Requests for Nonexistent Documents

--- cms.rb
+++ cms.rb
@@ -2,18 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
 root = File.expand_path("..", __FILE__)
 
 get "/" do
   @files = Dir.glob(root + "/data/*").map do |path|
     File.basename(path)
   end
+  
   erb :index
 end
 
 get "/:filename" do
   file_path = root + "/data/" + params[:filename]
 
-  headers["Content-Type"] = "text/plain"
-  File.read(file_path)
+  if File.exist?(file_path)
+    headers["Content-Type"] = "text/plain"
+    File.read(file_path)
+  else
+    session[:error] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -22,11 +22,22 @@ class CMSTest < Minitest::Test
     assert_includes last_response.body, "history.txt"
   end
 
-  def test_filename
+  def test_viewing_text_document
     get "/history.txt"
 
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes last_response.body, "Yukihiro Matsumoto dreams up Ruby"
+  end
+
+  def test_document_not_found
+    get "/notafile.ext"
+
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "notafile.ext does not exist."
   end
 end
